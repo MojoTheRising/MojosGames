77e8b0e3cc7ac3cac9a3b3c990cc2a66f582c24d
MojoTheRising <mojotherising@gmail.com>
2025-02-19 12:12:38 -0500

    Update engarde.py

--- engarde.py
+++ engarde.py
@@ -138,24 +138,30 @@ class Game:
 
         self.show_hand()
 
-    def cpu_move(self, value):
-        move_left = self.cpu.position - value
-        move_right = self.cpu.position + value
+def cpu_move(self, value):
+    move_left = self.cpu.position - value
+    move_right = self.cpu.position + value
 
-        if move_right == self.player.position:
-            self.cpu.position = move_right
-            self.update_board()
-            self.end_game("CPU wins!")
-        elif move_left == self.player.position:
-            self.cpu.position = move_left
-            self.update_board()
-            self.end_game("CPU wins!")
-        elif 0 <= move_right < 23 and move_right < self.player.position:
-            self.cpu.position = move_right
-        else:
-            self.cpu.position = move_left
-
+    if move_right == self.player.position:
+        self.cpu.position = move_right
+        if len(self.cpu.hand) > 0:
+            discarded_card = self.cpu.play_card(random.randint(0, len(self.cpu.hand) - 1))
+            print(f'CPU discards {discarded_card} to stay in the game.')
         self.update_board()
+        self.end_game("CPU wins!")
+    elif move_left == self.player.position:
+        self.cpu.position = move_left
+        if len(self.cpu.hand) > 0:
+            discarded_card = self.cpu.play_card(random.randint(0, len(self.cpu.hand) - 1))
+            print(f'CPU discards {discarded_card} to stay in the game.')
+        self.update_board()
+        self.end_game("CPU wins!")
+    elif 0 <= move_right < 23 and move_right < self.player.position:
+        self.cpu.position = move_right
+    else:
+        self.cpu.position = move_left
+
+    self.update_board()
 
     def move_player(self, player, value):
         new_pos = player.position + value
@@ -163,15 +169,22 @@ class Game:
             if 0 <= new_pos < 23:
                 player.position = new_pos
                 if player.position == self.cpu.position:
+                    # Add logic to discard a random card if about to lose
+                    if len(player.hand) > 0:
+                        discarded_card = player.play_card(random.randint(0, len(player.hand) - 1))
+                        print(f'{player.name} discards {discarded_card} to stay in the game.')
                     self.update_board()
                     self.end_game("Player wins!")
         else:
             if 0 <= new_pos < 23:
                 player.position = new_pos
                 if player.position == self.player.position:
+                    # Add logic to discard a random card if about to lose
+                    if len(player.hand) > 0:
+                        discarded_card = player.play_card(random.randint(0, len(player.hand) - 1))
+                        print(f'{player.name} discards {discarded_card} to stay in the game.')
                     self.update_board()
                     self.end_game("CPU wins!")
-
         self.update_board()
 
     def end_game(self, message):
